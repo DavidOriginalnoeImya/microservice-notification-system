9e343c8d6eedd879cd946d2b4369af30337f3a22
DavidOriginalnoeImya <abulashvili@inbox.ru>
2023-09-10 19:15:10 +0300

    feat: переименовать таблицу

--- registration-service/src/main/java/edu/home/registrationservice/data/service/DomainApp.java
+++ registration-service/src/main/java/edu/home/registrationservice/data/service/DomainApp.java
@@ -3,8 +3,10 @@ package edu.home.registrationservice.data.service;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "service")
 public class DomainApp {
 
     @Id
