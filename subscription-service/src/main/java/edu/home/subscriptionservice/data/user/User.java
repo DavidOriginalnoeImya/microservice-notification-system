08022f59ddc9c2f3aa7aa52d814157da109be4ed
DavidOriginalnoeImya <abulashvili@inbox.ru>
2023-09-17 16:25:12 +0400

    feat: добавить сущность пользователя

---
+++ subscription-service/src/main/java/edu/home/subscriptionservice/data/user/User.java
@@ -0,0 +1,32 @@
+package edu.home.subscriptionservice.data.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String guid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+}
---
+++ subscription-service/src/main/java/edu/home/subscriptionservice/data/user/UserRepository.java
@@ -0,0 +1,8 @@
+package edu.home.subscriptionservice.data.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
