de7257e64aed7f5c37bc085b1358ceb4f558c73e
DavidOriginalnoeImya <abulashvili@inbox.ru>
2023-09-17 20:14:12 +0400

    feat: переименовать таблицу

--- subscription-service/src/main/java/edu/home/subscriptionservice/data/user/User.java
+++ subscription-service/src/main/java/edu/home/subscriptionservice/data/user/User.java
@@ -3,8 +3,10 @@ package edu.home.subscriptionservice.data.user;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "ns_user")
 public class User {
 
     @Id
