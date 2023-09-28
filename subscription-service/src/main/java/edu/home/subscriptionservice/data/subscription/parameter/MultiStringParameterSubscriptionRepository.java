298833c4bab807a45f87a2e40b44392653a046f0
DavidOriginalnoeImya <abulashvili@inbox.ru>
2023-09-28 16:53:23 +0400

    feat: реализовать подписку на многострочный параметр

--- subscription-service/src/main/java/edu/home/subscriptionservice/data/subscription/parameter/MultiStringParameterSubscriptionRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.home.subscriptionservice.data.subscription.parameter;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MultiStringParameterSubscriptionRepository
-        extends IParameterSubscriptionRepository<MultiStringParameterSubscription> {
-}
--- subscription-service/src/main/java/edu/home/subscriptionservice/data/subscription/parameter/ParameterSubscriptionRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.home.subscriptionservice.data.subscription.parameter;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ParameterSubscriptionRepository
-        extends IParameterSubscriptionRepository<ParameterSubscription> {
-}
