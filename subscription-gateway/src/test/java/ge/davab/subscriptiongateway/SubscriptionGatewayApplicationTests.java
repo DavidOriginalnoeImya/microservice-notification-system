feb083e8745c32437941fd55acdd694511fe8ca0
DavidOriginalnoeImya <abulashvili@inbox.ru>
2024-01-11 14:38:36 +0400

    feat: инициализировать шлюз для подписки

---
+++ subscription-gateway/src/test/java/ge/davab/subscriptiongateway/SubscriptionGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.davab.subscriptiongateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
